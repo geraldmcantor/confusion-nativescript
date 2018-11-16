b79f485576c54527b3f22ff60853f71cc19bd703
Gerald Cantor <gcantor@cisco.com>
2018-11-15 21:10:34 -0600

    Storage

--- src/app/shared/baseurl.js
+++ src/app/shared/baseurl.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.baseURL = "http://192.168.0.14:3000/";
+exports.baseURL = "http://192.168.0.26:3000/";
 //export const baseURL = "http://10.152.244.58:3000/";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmFzZXVybC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImJhc2V1cmwudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBYSxRQUFBLE9BQU8sR0FBRywyQkFBMkIsQ0FBQztBQUNuRCxzREFBc0QiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgYmFzZVVSTCA9IFwiaHR0cDovLzE5Mi4xNjguMC4xNDozMDAwL1wiO1xuLy9leHBvcnQgY29uc3QgYmFzZVVSTCA9IFwiaHR0cDovLzEwLjE1Mi4yNDQuNTg6MzAwMC9cIjtcbiJdfQ==
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmFzZXVybC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImJhc2V1cmwudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFBYSxRQUFBLE9BQU8sR0FBRywyQkFBMkIsQ0FBQztBQUNuRCxzREFBc0QiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgY29uc3QgYmFzZVVSTCA9IFwiaHR0cDovLzE5Mi4xNjguMC4yNjozMDAwL1wiO1xuLy9leHBvcnQgY29uc3QgYmFzZVVSTCA9IFwiaHR0cDovLzEwLjE1Mi4yNDQuNTg6MzAwMC9cIjtcbiJdfQ==
